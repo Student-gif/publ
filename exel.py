3d41c026f7073f5350b288e1240e8b61026d047b
Student <asakrislanov@gmail.com>
2022-02-02 19:12:02 +0300

    as

--- exel.py
+++ exel.py
@@ -1,4 +1,8 @@
-from tokenize import group
+
+
+import os
+from re import I
+
 from QListenW import QListensW,lessonData
 import sqlite3
 import sys
@@ -6,7 +10,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.Qt import *
-from Logick import Prepods
 conn = sqlite3.connect("exl.db")
 cursor = conn.cursor()
  
@@ -28,8 +31,12 @@ class Table(QWidget):
             self.tableWidget.setRowHeight(i,80)
 
                  # Установить горизонтальный заголовок таблицы
-        for i in range(51):
+        for i in range(2,52):
             self.tableWidget.setColumnWidth(i,80)
+        self.tableWidget.setColumnWidth(1,60)
+        self.tableWidget.setColumnWidth(0,40)
+        self.tableWidget.setSpan(0,0,1,2)
+
 
         thing1 = 1  
         #color cells
@@ -42,15 +49,19 @@ class Table(QWidget):
             self.tableWidget.item(thing1,0).setFont(QFont("Arial", 16))
             self.tableWidget.item(thing1, 0).setBackground(QColor(0,160,0))      
             thing1 += 7
+        #присвоение табличного виджета
+
         for i in range(2,52):
             for g in range(1,43):
-                self.tableWidget.setCellWidget(g,i,QListensW(lessonData(teacher='Преподователь',auditory='аудитория',lesson='предметc')))
+                self.tableWidget.setCellWidget(g,i,QListensW(lessonData()))
         
         #second column color and №
         for i in range(2,43,7):
             for j in range(0,7):
+                
                 self.tableWidget.setItem(i+j-1, 1, QTableWidgetItem())
                 self.tableWidget.item(i+j-1,1).setText(str(j+1))
+                self.tableWidget.item(i+j-1,1).setTextAlignment(Qt.AlignVCenter|Qt.AlignCenter)
         c = cursor.execute('SELECT  * FROM GROUPS')
         
         
@@ -90,19 +101,44 @@ class Table(QWidget):
         item1 = menu.addAction (u'копировать')
         item2 = menu.addAction (u'вставить')
         item3 = menu.addAction (u'закрыть')
-        item1 = menu.addAction (u'Внести')
         action = menu.exec_(self.tableWidget.mapToGlobal(pos))
                          # Показать текст данных выбранной строки
+
+        
+        index = self.tableWidget.selectedIndexes()
         if action == item1:
-                print ('Вы выбрали первый вариант, текущее содержание текста строки:', ),
-                self.tableWidget.selectionModel().selection().indexes()
+            
+            self.index2 = self.tableWidget.cellWidget(index[0].row(),index[0].column())
+            
+           
+            
+           
+        #       print ('Вы выбрали первый вариант, текущее содержание текста строки:', ),
+        #       c=self.tableWidget.selectedIndexes()
+        #       if c.__len__()<2:
+        #           print(c[0].column())
+        #           h = QListensW.staticData.auditory
+        #           
+        #           print(h)
+        #       else:
+        #           pass
+
+        
+                
+
+
 
         if action == item2:
-                                 print ('Вы выбрали второй вариант, текущее содержание текста строки:',)
+            print ('Вы выбрали второй вариант, текущее содержание текста строки:',)
+            self.v = self.tableWidget.setCellWidget(index[0].column(),index[0].row(),self.index2.)
+            print(self.v)
+                                 
                      
         if action == item3:
                                  print ('Вы выбрали третий вариант, текущее содержание текста строки:', ),
- 
+def addToClipBoard(less:lessonData):
+    command="echo "+less.__str__()+" | click"
+    os.system(command=command)
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     example = Table()
